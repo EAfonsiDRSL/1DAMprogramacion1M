5da6dd086f7d4044aed785ec9ef8fbae700ddd09
EliotAlfonsoAriasO <ariasortega225@gmail.com>
2022-10-07 14:04:15 +0200

    subida 07/10

---
+++ src/EjercicioGrupal/AplicarLoAprendido.java
@@ -0,0 +1,68 @@
+package EjercicioGrupal;
+
+import java.util.Scanner;
+
+public class AplicarLoAprendido {
+    public static void main(String[] args) {
+
+
+    //declaramos las variables
+    final String SALUDO = "Hola a todos";
+    String acotacion = "*musica epica*";
+
+        System.out.println(SALUDO);
+        System.out.println("¿Como estamos clase?");
+        System.out.print("Hoy vamos a aprender las reglas basicas del ajedrez");
+        System.out.println(", creeis que lo hemos logrado? ");
+        System.out.print("Pues por su puesto, por algo estamos aqui, ");
+        System.out.println("ahora estais a punto de presenciar nuestro arduo y duro trabajo que hemos conseguido");
+        System.out.println("con el sudor de nuestra frente y sacrificio hacia nuestro ser.");
+        System.out.println("Antes de que podais obserbar los resultados de nuestro asombroso trabajo, ");
+        System.out.println("quiero deciros que tengais un muy buen y de provecho fin de semana y resto de dia, ");
+        System.out.println("ahora si comenzemos. "+acotacion);
+        System.out.println("\n");
+        System.out.println("Esta es una torre: \u2656");
+        System.out.println("Esto es un caballo: \u2658");
+        System.out.println("Esto es un alfil: \u2657");
+        System.out.println("Esto es un peón: \u2659");
+        System.out.println("Esta es la reina: \u2655");
+        System.out.println("Y por último este es el rey: \u2656");
+        System.out.println("\n");
+        System.out.println("Explicacion de sus movimientos: ");
+
+        System.out.println("-torre: \u2656  ==> sus movimientos se basan moverse en linea recta \n hacia los lados");
+        System.out.println("-caballo: \u2658  ==> cuyos movimientos estan inspirados en la letra \'l\'");
+        System.out.println("-Alfil: \u2657  ==> son piezas que se mueven en diagonal");
+        System.out.println("-Reyna: \u2655  ==> es una pieza que se puede mover en cualquier direccion las \n casillas que quiera");
+        System.out.println("-Rey: \u2654  ==> es la pieza más importante y se mueve en cualquier direccion \n solo una casilla");
+        System.out.println("-Peon: \u2659  ==> sus movimientos son unicament una casillas hacia delante aunque \n al comer otra ficha solo puede hacerlo en diagonal");
+        System.out.println("\n");
+        System.out.println("Este es el terreno de juego, donde se disputan numerosas batallas sangrientas entre las piezas \n para conquistar el tablero.");
+        System.out.println("   "+"███"+"   "+"███"+"   "+"███"+"   "+"███");
+        System.out.println("███"+"   "+"███"+"   "+"███"+"   "+"███"+"   ");
+        System.out.println("   "+"███"+"   "+"███"+"   "+"███"+"   "+"███");
+        System.out.println("███"+"   "+"███"+"   "+"███"+"   "+"███"+"   ");
+        System.out.println("   "+"███"+"   "+"███"+"   "+"███"+"   "+"███");
+        System.out.println("███"+"   "+"███"+"   "+"███"+"   "+"███"+"   ");
+        System.out.println("   "+"███"+"   "+"███"+"   "+"███"+"   "+"███");
+        System.out.println("███"+"   "+"███"+"   "+"███"+"   "+"███"+"   ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+}
