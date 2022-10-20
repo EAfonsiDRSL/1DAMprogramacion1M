ee7f10e8a5afdfd2d7830300ab2101e763e0e3df
arias <ariasortega225@gmail.com>
2022-10-20 21:36:55 +0200

    sesion 20/10 v2

--- src/Sesion1710/Condicionales/Ejercicio6Condicionales.java
+++ src/Sesion1710/Condicionales/Ejercicio6Condicionales.java
@@ -3,8 +3,25 @@ package Sesion1710.Condicionales;
 
  */
 
+import java.util.Scanner;
+
 public class Ejercicio6Condicionales {
     public static void main(String[] args) {
+        // declaramos e inicializamos variables
+        final float g= 9.8f ;
+        float t = 0;
+        float h = 0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Para calcular el tiempo de caida de un objeto nesesitamos la altura de la que va a caer el objeto");
+        System.out.println("Porfavor introduzca la altura en metros");
+        h=sc.nextFloat();
+        if (h<0){
+            System.out.println("NO existe solucion real para raices negativas");
+        }else {
+            System.out.println("El tiempo en caer el objeto es de "+Math.sqrt((2*h)/g)+" segundos");}
+
 
     }
-}
+
+    }
+
--- src/Sesion1710/Condicionales/Ejercicio7y8Condicionales.java
+++ src/Sesion1710/Condicionales/Ejercicio7y8Condicionales.java
@@ -1,4 +1,36 @@
 package Sesion1710.Condicionales;
 
+import java.util.Scanner;
+
 public class Ejercicio7y8Condicionales {
-}
+    public static void main(String[] args) {
+        //declarar e inicializar variables
+        float nota1=0, nota2=0, nota3=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca la primera nota");
+        nota1 = sc.nextFloat();
+        System.out.println("Introduzca la segunda nota");
+        nota2 = sc.nextFloat();
+        System.out.println("Introduzca la tercera nota");
+        nota3 = sc.nextFloat();
+        float media = (nota1+nota2+nota3)/3;
+        if (nota1<0|nota2<0|nota3<0) {
+            System.out.println("Porfavor introduzca un valor correcto");
+        }else if (nota1>10|nota2>10|nota3>10){
+            System.out.println("Introduzca un valor entre el 0 y el 10");
+        }else {
+            System.out.print("Su nota media es "+media );}
+        if (media>=0&&media<=4.99){
+            System.out.print(" insuficiente");}
+        else if (media>=5&&media<=5.99){
+            System.out.print(" suficiente");}
+        else if (media>=6&&media<=6.99){
+            System.out.print(" bien");}
+        else if (media>=7&&media<=8.99){
+            System.out.print(" notable");}
+        else if (media>=9&&media<=10){
+            System.out.print(" sobresaliente");}
+    }
+
+    }
+
