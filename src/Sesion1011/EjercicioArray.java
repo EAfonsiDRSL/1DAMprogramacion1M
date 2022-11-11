90e8acd2de7b886d80eaba85fb258907b48c9fd2
EliotAlfonsoAriasO <ariasortega225@gmail.com>
2022-11-11 14:06:20 +0100

    sesion 11/11

--- src/Sesion1011/EjercicioArray.java
+++ src/Sesion1011/EjercicioArray.java
@@ -15,18 +15,18 @@ public class EjercicioArray {
 
         for (int i=0;i< bidimensional1.length;i++){
             for (int j=0;j< bidimensional1.length;j++){
-                System.out.println(bidimensional1[i][j]);
+                System.out.print(bidimensional1[i][j]+" , ");
             }
         }
-        System.out.println("Array segunda forma");
+
         int[][]bidimensional2={{1,8},{9,10}};
 
         for (int i=0;i<bidimensional2.length;i++){
             for (int j=0;j< bidimensional2.length;j++){
-                System.out.println(bidimensional2[i][j]);
+                System.out.print(bidimensional2[i][j]+" , ");
             }
         }
-        System.out.println("Array tercera forma");
+        System.out.println("\nArray tercera forma");
         int[][]bidimensional3 = new int [2][2];
         Scanner sc= new Scanner(System.in);
         for (int i=0;i<bidimensional3.length;i++){
@@ -34,28 +34,56 @@ public class EjercicioArray {
                 System.out.println("Introduce un numero");
                 bidimensional3[i][j]=sc.nextInt();
             }
+        }System.out.println("Sus numeros son:");
+        for (int i=0;i<bidimensional3.length;i++){
+            for (int j=0;j<bidimensional3.length;j++){
+
+                System.out.print(bidimensional3[i][j]+" , ");
+            }
         }
 
+
+        System.out.println("\nPrimera forma");
         int [][]matriz2x3;
         matriz2x3=new int[2][3];
         matriz2x3[0][0]=2;
-        matriz2x3[0][1]=2;
-        matriz2x3[0][2]=2;
-        matriz2x3[1][0]=2;
-        matriz2x3[1][1]=2;
-        matriz2x3[1][2]=2;
+        matriz2x3[0][1]=4;
+        matriz2x3[0][2]=3;
+        matriz2x3[1][0]=8;
+        matriz2x3[1][1]=9;
+        matriz2x3[1][2]=210;
 
+        for (int i=0;i< matriz2x3.length;i++){
+            for (int j=0;j< matriz2x3[i].length;j++){
+                System.out.print(matriz2x3[i][j]+" , ");
+            }
+        }
+
+
+        System.out.println("\nSegunda forma");
         int [][]matriz2x3v2={{5,6,8},{3,7,9}};
 
+        for (int i=0;i< matriz2x3v2.length;i++){
+            for (int j=0;j< matriz2x3v2[i].length;j++){
+                System.out.print(matriz2x3v2[i][j]+" , ");
+            }
+        }
 
+
+        System.out.println("\nTercera forma");
         int [][]matriz2x3v3=new int [2][3];
         for (int I=0;I<matriz2x3v3.length;I++){
             for (int J=0;J<matriz2x3v3[I].length;J++){
                 System.out.println("Introduzca un numero");
                 matriz2x3v3[I][J]=sc.nextInt();
             }
+        }System.out.println("Sus numeros son:");
+        for (int I=0;I<matriz2x3v3.length;I++){
+            System.out.print("\n");
+            for (int J=0;J<matriz2x3v3[I].length;J++){
+                System.out.print(matriz2x3v3[I][J]+" , ");
+            }
         }
-
 
 
     }
