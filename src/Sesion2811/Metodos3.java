f6059d534ed927ddf48315310b239640be3e0b79
EliotAlfonsoAriasO <ariasortega225@gmail.com>
2022-11-30 10:30:15 +0100

    sesion 30/11

--- src/Sesion2811/Metodos3.java
+++ src/Sesion2811/Metodos3.java
@@ -1,5 +1,6 @@
 package Sesion2811;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Metodos3 {
@@ -9,20 +10,49 @@ public class Metodos3 {
         System.out.println("Introduzca una palabra");
         Scanner sc = new Scanner(System.in);
         palabra = sc.next();
-        int resultado = longitud_String(palabra);
-        System.out.println("Su palabra tiene "+longitud_String(palabra)+" letras");
-        System.out.println("Su inicial es "+inicial(palabra));
-        System.out.println("Su ultima letra es "+ultima_letra(palabra));
-        System.out.println("Su palabra en mayusculas es "+mayus(palabra));
-        System.out.println("Su palabra en minusculas es "+minus(palabra));
-        System.out.println("Su nueva palabra es "+cambio(palabra));
+
+        System.out.println("Menú de funcionalidades");
+        System.out.println("1- Calcular la longitud de la palabra");
+        System.out.println("2- Mostrar la inicial de la palabra");
+        System.out.println("3- Mostrar la última letra de la palabra");
+        System.out.println("4- Quitar espacios si hubiese");
+        System.out.println("5- Mostrar la palabra en mayusculas");
+        System.out.println("6- Mostrar la palabra en minusculas");
+        System.out.println("7- Reemplazar una letra por otra");
+        System.out.println("8- Indicar si el String está vacio");
+        System.out.println("9- Pasar a un array de char");
+
+        System.out.println("Introduzca la acción que desea realizar");
+        int opcion = sc .nextInt();
+
+        switch (opcion){
+            case 1:System.out.println("Su palabra tiene "+longitud_String(palabra)+" letras");break;
+            case 2:System.out.println("Su inicial es "+inicial(palabra));break;
+            case 3:System.out.println("Su ultima letra es "+ultima_letra(palabra));break;
+            case 4:quitar_espacios(palabra);break;
+            case 5:System.out.println("Su palabra en mayusculas es "+mayus(palabra));break;
+            case 6:minus(palabra);break;
+            case 7:System.out.println("Su nueva palabra es "+cambio(palabra));break;
+            case 8:vacio(palabra);break;
+            case 9:
+                char[] array = array_de_char(palabra);
+                System.out.println(Arrays.toString(array));break;
+            default:System.out.println("Has introducido una opcion incorrecta");break;
+
+
+        }
+
+
 
     }
     private static int longitud_String (String palabra){
         int resultado = 0;
         resultado=palabra.length();
         return resultado;
+
     }
+
+
     private static char inicial (String palabra){
         char inicial;
         inicial=palabra.charAt(0);
@@ -33,20 +63,21 @@ public class Metodos3 {
         ultima_letra=palabra.charAt(palabra.length()-1);
         return ultima_letra;
     }
-   // private static void  (String palabra){
-       // String espacios = " ";
-      //  espacios=palabra.trim().length();
-       // return espacios;
-   // }
+   private static void quitar_espacios (String palabra){
+       String espacios = " ";
+       espacios=palabra.trim();
+       System.out.println("Su palabra sin espacios es");
+       System.out.println(espacios);
+   }
     private static String mayus (String palabra){
         String mayus;
         mayus = palabra.toUpperCase();
         return mayus;
     }
-    private static String minus(String palabra){
+    private static void minus(String palabra){
         String minus;
         minus = palabra.toLowerCase();
-        return minus;
+        System.out.println("Se palabra en minusculas es "+minus);
 
     }
     private static String cambio (String palabra){
@@ -62,8 +93,14 @@ public class Metodos3 {
         cambio = palabra.replace(old_char,new_char);
         return cambio;
     }
-    //private static String array_de_char (String palabra){
-      //  String array_de_char=" ";
+    private static void vacio (String palabra){
 
-    //}
+        System.out.println("¿La palabra introducida esta vacia? "+palabra.isEmpty());
+
+    }
+    private static char [] array_de_char (String palabra){
+        char [] array_de_char;
+        array_de_char = palabra.toCharArray();
+        return array_de_char;
+    }
 }
