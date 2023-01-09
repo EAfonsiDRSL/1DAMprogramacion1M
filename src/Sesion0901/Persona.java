142ada769da92385fae7754b1ef44ae6070146a6
EliotAlfonsoAriasO <ariasortega225@gmail.com>
2023-01-09 14:14:11 +0100

    Sesion 9/01

---
+++ src/Sesion0901/Persona.java
@@ -0,0 +1,24 @@
+package Sesion0901;
+
+public class Persona {
+
+    //atributos
+    private String dni;
+    private String nombre="Eliot";
+    private String apellidos;
+
+    //metodos
+
+    public Persona(){
+        apellidos = "Arias";
+    }
+
+    public String getDni() {
+
+        return dni;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+}
---
+++ src/Sesion0901/UsoPersona.java
@@ -0,0 +1,9 @@
+package Sesion0901;
+
+public class UsoPersona {
+
+
+    Persona p1 = new Persona();
+    p1.dni = "3226625x";
+
+}
