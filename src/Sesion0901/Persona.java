b0fec2f6654fe20478cb618679b1906656a4f3f0
EliotAlfonsoAriasO <ariasortega225@gmail.com>
2023-01-11 10:17:39 +0100

    Sesion 11/01

--- src/Sesion0901/Persona.java
+++ src/Sesion0901/Persona.java
@@ -4,13 +4,30 @@ public class Persona {
 
     //atributos
     private String dni;
-    private String nombre="Eliot";
+    private String nombre;
     private String apellidos;
+    private int edad;
 
     //metodos
 
     public Persona(){
-        apellidos = "Arias";
+
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getDni() {
@@ -18,7 +35,16 @@ public class Persona {
         return dni;
     }
 
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
     public String getApellidos() {
         return apellidos;
     }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
 }
+
--- src/Sesion0901/UsoPersona.java
+++ src/Sesion0901/UsoPersona.java
@@ -2,8 +2,22 @@ package Sesion0901;
 
 public class UsoPersona {
 
+    public static void main(String[] args) {
 
-    Persona p1 = new Persona();
-    p1.dni = "3226625x";
+        Persona p1 = new Persona();
+
+        System.out.println("El nombre de la primera persona es: "+p1.getNombre());
+        System.out.println("El apellido de la primera persona es: "+p1.getApellidos());
+        System.out.println("El dni de la primera persona es: "+p1.getDni());
+        System.out.println("La edad de la persona es: "+p1.getEdad());
+
+        p1.setDni("12345678D");
+        p1.setNombre("Eliot");
+        p1.setApellidos("Arias");
+
+
+        Persona p2 = new Persona();
+    }
+
 
 }
